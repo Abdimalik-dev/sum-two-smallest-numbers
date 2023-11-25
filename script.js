aad2b8d2a85636ac36401ff56e687b5713a49d15
Abdimalik-dev <cabdixuseen076@gmail.com>
2023-11-25 23:53:42 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Good Luck 💪🏾
+function sumTwoSmallestNums(AK) {
+    AK.sort(function(c, d) {
+      return c - d;
+    });
+    var sum = AK[0] + AK[1];
+    return sum;
+  }
+  console.log(sumTwoSmallestNums([12, 5, 24, 55, 18]));
+  console.log(sumTwoSmallestNums([190, 300, 230, 800]));  
